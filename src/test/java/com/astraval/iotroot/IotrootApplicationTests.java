9a1289b5b718032d1c03aff6052c5ea8db7b5b39
rahul <rahul.iotecotech@gmail.com>
2025-09-07 12:42:25 +0530

    project init

---
+++ src/test/java/com/astraval/iotroot/IotrootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astraval.iotroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotrootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
