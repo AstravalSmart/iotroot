3f8e12e001513b9e1f11784a578a13eb5c49dd4f
karthikeyan <karthikeyan2110641@ssn.edu.in>
2025-10-07 19:35:30 +0530

    SignUp and Signin Api most used service auto selected

--- src/main/java/com/astraval/iotroot/config/WebConfig.java
+++ src/main/java/com/astraval/iotroot/config/WebConfig.java
@@ -15,8 +15,8 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173", "http://192.168.1.8:5173")
-                        .allowedMethods("*")
-                        .allowCredentials(true);
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(false);
             }
         };
     }
